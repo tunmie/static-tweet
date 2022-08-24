648d4cf3677f2bba12e217400576e1db8038a097
tunmie <victoroluwasomidotun@gmail.com>
2022-08-24 03:40:24 +0100

    ✨ feat(tweet component): added buttons
    reply, retweet, like and more options button added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ function Tweet() {
   return (
     <div className="tweet">
       <Avatar />
-      Tweet
+      <div className="content">
+        <Author />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
     </div>
   );
 }
@@ -15,8 +24,45 @@ function Avatar() {
   return (
     <img src="https://www.gravatar.com/avatar/nothing" alt="avatar" className="avatar"/>
   )
-}
+};
 
+
+function Message() { 
+  return (
+    <div className="message">
+      This is less than 140 characters.
+    </div>
+  )
+};
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  )
+};
+
+const Time = () => (
+  <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet reply-button" />
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button"></i>
+);
 
 const container = document.querySelector('#root')
 const root = createRoot(container)
